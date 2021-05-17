fe17c4c2db3a62ddb7f0fe7e2e9584dedc6b4284
Karunia Sentosa <s32190083@student.ubm.ac.id>
2021-05-17 15:51:06 +0700

    firebase settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { AuthenticationGuard } from "./authentication.guard";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: AppComponent, canActivate: [AuthenticationGuard]},
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule} from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { LoginComponent } from './login/login.component';
+import { AuthenticationService } from "./authentication.service";
+import { CookieService } from "ngx-cookie-service";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiZQGw04jNztI9YbRq-X9JcAW_wC6vk9s",
+  authDomain: "x-learner.firebaseapp.com",
+  projectId: "x-learner",
+  storageBucket: "x-learner.appspot.com",
+  messagingSenderId: "284545377521",
+  appId: "1:284545377521:web:7823615f0713dcc4b3b550"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [AuthenticationService, CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
